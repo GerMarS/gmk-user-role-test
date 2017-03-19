9109f1b5e9fe7ce9624a7cef55fc61b8bad1394d
GerMarS <gmartinezsoy@gmail.com>
2017-03-19 20:01:44 +0100

    Añadiendo vista de cabecera y cierre de página

---
+++ application/views/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+     <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>GMK Test</title>
+	 <!--link the bootstrap css file-->
+	 <link href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>" rel="stylesheet">
+	 <!--link nuestra propia hoja de estilo -->
+	 <link href="<?php echo base_url('assets/css/style.css'); ?>" rel="stylesheet">
+	 
+	 <!--load jQuery library-->
+	<script src="<?php echo base_url('assets/js/jquery-3.1.1.min.js'); ?>"></script>
+	<!--load bootstrap.js-->
+	<script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
+     
+</head>
+<body>
+
+<div class="page-header">
+	<div class="container">
+		<h1>GMK Medialab</h1>
+		<h3>Test de usuarios y roles</h3>
+	</div>
+</div>
+
+<div class="container">
+	<div class="margin20"></div>
+	<div class="row">
